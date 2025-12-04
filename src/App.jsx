71a24ba5837502673ae58b4eb66a48e0c4ba95ea
Mohammed Adetunji <theadetunji001@gmail.com>
2025-12-04 10:23:22 +0100

    Took a break to learn about states

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import MainElement from "./components/MainElement/MainElement";
 
 export default function App() {
   return (
-    <div className="container">
+    <>
       <Header />
-      <MainElement />
-    </div>
+      <div className="container">
+        <MainElement />
+      </div>
+    </>
   );
 }
--- src/components/MainElement/MainElement.jsx
+++ src/components/MainElement/MainElement.jsx
@@ -1,5 +1,74 @@
 import "../MainElement/MainElement.css";
+import { Plus } from "lucide-react";
+import { Minus } from "lucide-react";
+
+import React from "react";
 
 export default function MainElement() {
-  return <h1>Hello Main</h1>;
+  const [isCount, setIsCount] = React.useState(0);
+
+  function add() {
+    setIsCount((prevCount) => prevCount + 1);
+  }
+
+  function minus() {
+    if (isCount === 0) {
+      isCount;
+    } else {
+      setIsCount((prevCount) => prevCount - 1);
+    }
+  }
+
+  // const ingredients = ["oregano", "chilli", "pasta"];
+
+  // const ingredientItemList = ingredients.map((ingredient) => {
+  //   return <li key={ingredient}>{ingredient}</li>;
+  // });
+
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   const formData = new FormData(e.currentTarget);
+  //   const newIngredient = formData.get("ingredient");
+  //   // console.log(newIngredient);
+  //   ingredients.push(newIngredient);
+  //   console.log(ingredients);
+  // }
+
+  return (
+    <main className="container">
+      <h1>How many times will Bob say "state" in this section?</h1>
+      <div className="counter">
+        <button
+          onClick={minus}
+          aria-label="Decrease count"
+          className="operator minus"
+        >
+          <Minus />
+        </button>
+        <h2 className="value">{isCount}</h2>
+        <button
+          onClick={add}
+          aria-label="Increase count"
+          className="operator plus"
+        >
+          <Plus />
+        </button>
+      </div>
+    </main>
+    // <main className="main">
+    //   <form onClick={handleSubmit} className="form-container">
+    //     <input
+    //       type="text"
+    //       aria-label="Add ingredients"
+    //       placeholder="e.g. oregano"
+    //       name="ingredient"
+    //     />
+    //     <button type="submit" className="button">
+    //       <Plus aria-hidden="true" size=".5rem" weight="bold" />
+    //       Add ingredient
+    //     </button>
+    //   </form>
+    //   <ul>{ingredientItemList}</ul>
+    // </main>
+  );
 }
